d037368b5a67d8b35c358ca7bc1911ef2027f994
NathanPesneau <npesneau@MacBook-Pro-de-Pesneau.local>
2023-09-05 13:59:19 +0200

    ajout des emojis pour cacher les mots

--- sweetWeb/script.js
+++ sweetWeb/script.js
@@ -1,3 +1,6 @@
+// Importe le tableau contenant les mots méchants
+// import { array } from "./dictionary";
+
 // Insère une chaîne de caractère dans une Regex
 function insertInRegex(textToAdd) {
     const testVar = textToAdd;
@@ -5,7 +8,7 @@ function insertInRegex(textToAdd) {
     return regex
 }
 
-// fonction qui parcoure tous les éléments du DOM, 
+// fonction qui parcoure tous les éléments du DOM
 function replaceText(element, toReplace) {
     // vérifie si l'élément parcouru a des noeuds enfants
     if (element.hasChildNodes()) {
@@ -16,7 +19,17 @@ function replaceText(element, toReplace) {
         // Si l'élément du noeud parcouru est bien de type texte, rentre dans la boucle
     } else if (element.nodeType === Text.TEXT_NODE) {
         // on remplace le texte qui correspond au regex
-        element.textContent = element.textContent.replace(toReplace, '***');
+        element.textContent = element.textContent.replace(toReplace, '💗💗💗💗💗');
+
+        // Option longueur mot (marche pas pour l'instant)
+        // const wordLength = element.textContent.length;
+        // console.log(wordLength)
+        // let newWord = ''
+        // for (i = 0; i < wordLength; i++) {
+        //     newWord += '*'
+        // }
+        // console.log(newWord)
+
     }
 }
 
