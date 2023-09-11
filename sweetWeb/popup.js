7e192ff74d553eec51eb77a723b2a4f2977d705c
NathanPesneau <npesneau@MacBook-Pro-de-Pesneau.local>
2023-09-11 11:48:37 +0200

    ajout fonction bouton check par défaut

--- sweetWeb/popup.js
+++ sweetWeb/popup.js
@@ -1,10 +1,18 @@
-(()=>{
+(() => {
     const button = document.getElementById('myCheckbox');
     button.addEventListener('click', function () {
         console.log('clicked')
 
-        chrome.runtime.sendMessage({greeting:'Hello'}).then(response =>
+        chrome.runtime.sendMessage({ greeting: 'Hello' }).then(response =>
             console.log(response))
-        })
-    })()
-  
+    })
+})()
+
+function buttonSwitch() {
+    console.log('switch')
+    document.querySelector('#changeButtonState').innerHTML = `    <input id="myCheckbox" type="checkbox" value="yes" checked />
+    <span class="slider round"></span>`
+}
+
+buttonSwitch()
+
