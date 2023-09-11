58cc8c1b392418aecdc99a37044b6a822cd92221
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-09-11 11:22:43 +0200

    bouton on/off sur popup

--- sweetWeb/popup.js
+++ sweetWeb/popup.js
@@ -2,9 +2,23 @@
     const button = document.getElementById('myCheckbox');
     button.addEventListener('click', function () {
         console.log('clicked')
-
+        enabledDisabled() 
+         //appel de la fonction 
         chrome.runtime.sendMessage({greeting:'Hello'}).then(response =>
             console.log(response))
         })
     })()
-  
+
+
+function enabledDisabled (){
+    chrome.storage.local.get(["enabled"]).then((result) => {
+        if (result.enabled === true){
+            chrome.storage.local.set({ enabled: false}).then(()=> {
+                console.log(result.enabled)
+            })}
+        else {
+            chrome.storage.local.set({ enabled: true}).then(()=> {
+                console.log(result.enabled)
+            })}
+        })}
+
